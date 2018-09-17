56f52c20d3eb80d77d0ebba7b723e269ac661400
bmartinko <bennett.martinko@gmail.com>
2018-09-17 11:22:38 -0400

    Commit #1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    Scanner kbScanner = new Scanner(System.in);
+        System.out.println("First Word:");
+        String firstWord = kbScanner.nextLine();
+        System.out.println("Second Word:");
+        String secondWord = kbScanner.nextLine();
+        System.out.println("Is this an anagram");
+        if (firstWord.length() != secondWord.length()){
+            System.out.println("False! " + firstWord + " and " + secondWord + " are not anagrams!");
+        }else {
+            
+        }
+
     }
 }
