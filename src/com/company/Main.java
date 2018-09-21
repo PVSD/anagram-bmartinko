9b1d6c0ae590c4e675519d71b7934ac8430f1a0c
bmartinko <bennett.martinko@gmail.com>
2018-09-21 01:00:31 -0400

    'Did you ever hear the Tragedy of Darth Plagueis the wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. It's ironic he could save others from death, but not himself.'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,35 @@
 package com.company;
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
     Scanner kbScanner = new Scanner(System.in);
         System.out.println("First Word:");
-        String firstWord = kbScanner.nextLine();
+        String firstWord = kbScanner.next();
         System.out.println("Second Word:");
-        String secondWord = kbScanner.nextLine();
-        System.out.println("Is this an anagram");
-        if (firstWord.length() != secondWord.length()){
-            System.out.println("False! " + firstWord + " and " + secondWord + " are not anagrams!");
-        }else {
-            
+        String secondWord = kbScanner.next();
+
+        if (firstWord.length() == secondWord.length()) {
+
+            char[] arrayOne = firstWord.toCharArray();
+            char[] arrayTwo = secondWord.toCharArray();
+
+            int count = 0;
+            for (int i = 0; i < arrayOne.length; i++) {
+                if (arrayOne[i] == arrayTwo[i])
+                    count++;
+
+            }
+            if (count == arrayTwo.length) ;
+                System.out.println(firstWord + " and " + secondWord + " are anagrams!");
+            }
+        else {
+            System.out.println(firstWord + " and " + secondWord + " are not anagrams!");
+        }
+
         }
 
     }
-}
+
